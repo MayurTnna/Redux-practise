96ffbf05b03de2651390ae3a3c7a5d5624366614
mayurtnn <mayurt@zignuts.com>
2023-03-03 16:34:47 +0530

    harry's project started

---
+++ redux-harry/src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+        <h1>Buy now!</h1>
+        <button className='btn btn-primary'>-</button> &nbsp;Add to cart &nbsp;
+        <button className='btn btn-primary'>+</button>
+    </div>
+  )
+}
+
+export default Shop
